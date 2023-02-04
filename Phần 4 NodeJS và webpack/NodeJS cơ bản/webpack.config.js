44a39b798b93cd05f348f31410ae6283aaaee4dc
Hieu <nguyenhieu131020@gmail.com>
2023-02-04 11:58:02 +0700

    config file html webpack

--- Phần 4 NodeJS và webpack/NodeJS cơ bản/webpack.config.js
+++ Phần 4 NodeJS và webpack/NodeJS cơ bản/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
-console.log("path_resole", path.resolve())
-console.log("dirname is",__dirname)
+const HtmlwebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -12,8 +11,15 @@ module.exports = {
     rules:[
       {
         test:/\.s[ac]ss|css$/,
-        use:['style-loader','css-loader']
+        use:['style-loader','css-loader','sass-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/template.html'
+    })
+  ]
 }

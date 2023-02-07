3856cb554c311bf89523ac91eb3c064575a025ce
Hieu <nguyenhieu131020@gmail.com>
2023-02-07 14:58:34 +0700

    add sourcemap

--- Phần 4 NodeJS và webpack/NodeJS cơ bản/webpack.config.js
+++ Phần 4 NodeJS và webpack/NodeJS cơ bản/webpack.config.js
@@ -1,42 +1,46 @@
 const path = require('path')
 const HtmlwebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    clean: true,
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  },
-  module:{
-    rules:[
-      {
-        test:/\.s[ac]ss|css$/,
-        use:[MiniCssExtractPlugin.loader
-          ,'css-loader','sass-loader']
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin(
-      {
-        filename:'[name].[contenthash].css'
-      }
-    ),
-    new HtmlwebpackPlugin({
-      title: 'Webpack App',
-      filename: 'index.html',
-      template: 'src/template.html'
-    })
-  ],
-  devServer:{
-    static : {
-      directory: 'dist' // Đường dẫn tương đối đến thư mục chứa file index.html
+module.exports = (env) => {
+  const isDevelopement = Boolean(env.developement)
+  return {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      clean: true,
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js'
     },
-    port: 3000, // port thay cho port mặc định
-    open:true, // Mở trang webpack khi chạy trên terminal
-    compress:true,  // Bật Zip cho các tài nguyên
-    historyApiFallback:true 
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader
+            , 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin(
+        {
+          filename: '[name].[contenthash].css'
+        }
+      ),
+      new HtmlwebpackPlugin({
+        title: 'Webpack App',
+        filename: 'index.html',
+        template: 'src/template.html'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: 'dist' // Đường dẫn tương đối đến thư mục chứa file index.html
+      },
+      port: 3000, // port thay cho port mặc định
+      open: true, // Mở trang webpack khi chạy trên terminal
+      compress: true,  // Bật Zip cho các tài nguyên
+      historyApiFallback: true
+    }
   }
+
 }
